bbaf20ef462ae7bcd9504484e86804b5abb7a5ba
Boris Civcic <civcicboris93@gmail.com>
2019-07-05 20:20:10 +0200

    add new section

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Abril Fatface`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Source Sans Pro`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
