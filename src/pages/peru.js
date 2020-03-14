ee77b24672a426fbdb75ea2fcf48b317c2620d5c
Boris (fixman93) <fixman93@users.noreply.github.com>
2020-03-14 16:50:57 +0100

    Update peru.js

--- src/pages/peru.js
+++ src/pages/peru.js
@@ -12,7 +12,7 @@ const SecondPage = () => (
       special="SUMMER SPECIAL:"
       title="Visit Peru with Juan CCaihuari"
       bgImage="https://s3.amazonaws.com/greatguides-prod-media/Trips/Lares+Trek+Peru/20181005_145258+Med.jpeg"
-      description="Juan is a Peruvian Quechua whose great, great grandfather settled on, and was granted ownership of, the land around and including Choquequirao. Juan has started his own guide company which is the only tour company run by, and employing, the locals around Choquequirao. The guides, cooks, and muleteers are all local residents. He rents mules and horses from the locals, camps on their land to give them rental income, buys food grown in the villages along the trail, and is working on other ways to ensure the tourist dollars are widely distributed."
+      description="Juan  is a Peruvian Quechua whose great, great grandfather settled on, and was granted ownership of, the land around and including Choquequirao. Juan has started his own guide company which is the only tour company run by, and employing, the locals around Choquequirao. The guides, cooks, and muleteers are all local residents. He rents mules and horses from the locals, camps on their land to give them rental income, buys food grown in the villages along the trail, and is working on other ways to ensure the tourist dollars are widely distributed."
     />
     <Adventures />
     <Questions />
